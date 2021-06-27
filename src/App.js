0b99e3c610d224f955c883fafd789e392ab12344
Bogdan <bn.workdev@gmail.com>
2021-06-27 21:26:46 +0100

    finish the calculator

--- src/App.js
+++ src/App.js
@@ -1,114 +1,170 @@
 import React, { useEffect, useState } from 'react';
-import Result from './components/Result';
+import NumberFormat from 'react-number-format';
 
-import './App.css';
+import Result from './components/Result';
 import Button from './components/Button';
 
+import './App.css';
+
 const App = () => {
-  const [firstNumber, setFirstNumber] = useState('');
-  const [secondNumber, setSecondNumber] = useState('');
+  const [preState, setPreState] = useState('');
+  const [curState, setCurState] = useState('');
+  const [input, setInput] = useState('0');
+  const [operator, setOperator] = useState(null);
+  const [total, setTotal] = useState(false);
 
-  const [operation, setOperation] = useState('');
+  const inputNum = e => {
+    if (curState.includes('.') && e.target.innerText === '.') return;
 
-  const [result, setResult] = useState(0);
+    if (total) {
+      setPreState('');
+    }
+
+    curState ? setCurState(pre => pre + e.target.innerText) : setCurState(e.target.innerText);
+    setTotal(false);
+  };
 
   useEffect(() => {
-    console.log(result);
-  }, [result]);
+    setInput(curState);
+  }, [curState]);
 
-  const clickNumbers = val => {
-    if (operation === '') {
-      setFirstNumber(firstNumber + val);
+  useEffect(() => {
+    setInput('0');
+  }, []);
+
+  const operatorType = e => {
+    setTotal(false);
+    setOperator(e.target.innerText);
+    if (curState === '') return;
+    if (preState !== '') {
+      equals();
     } else {
-      setSecondNumber(secondNumber + val);
+      setPreState(curState);
+      setCurState('');
     }
   };
 
-  const clickOperation = val => {
-    setOperation(val);
-  };
+  const equals = e => {
+    if (e?.target.innerText === '=') {
+      setTotal(true);
+    }
+    let cal;
+    switch (operator) {
+      case '/':
+        cal = String(parseFloat(preState) / parseFloat(curState));
+        break;
 
-  const performOperation = () => {
-    // eslint-disable-next-line default-case
-    switch (operation) {
       case '+':
-        setResult(Number(firstNumber) + Number(secondNumber));
+        cal = String(parseFloat(preState) + parseFloat(curState));
+        break;
+      case 'X':
+        cal = String(parseFloat(preState) * parseFloat(curState));
         break;
       case '-':
-        setResult(Number(firstNumber) - Number(secondNumber));
+        cal = String(parseFloat(preState) - parseFloat(curState));
         break;
-      case '*':
-        setResult(Number(firstNumber) * Number(secondNumber));
-        break;
-      case '/':
-        setResult(Number(firstNumber) / Number(secondNumber));
-        break;
+      default:
+        return;
     }
-  };
-  const backspace = () => {
-    setResult(result.slice(0, -1));
+    setInput('');
+    setPreState(cal);
+    setCurState('');
   };
 
-  const clearInput = () => {
-    setResult('');
+  const minusPlus = () => {
+    if (curState.charAt(0) === '-') {
+      setCurState(curState.substring(1));
+    } else {
+      setCurState('-' + curState);
+    }
+  };
+
+  const percent = () => {
+    preState
+      ? setCurState(String((parseFloat(curState) / 100) * preState))
+      : setCurState(String(parseFloat(curState) / 100));
+  };
+
+  const backspace = () => {
+    setInput(input.slice(0, -1));
+  };
+
+  const reset = () => {
+    setPreState('');
+    setCurState('');
+    setInput('0');
   };
 
   return (
     <div className="container calculator">
       <div className="m-10 box-shadow rounded-md">
-        <Result value={result} />
+        <Result>
+          {input !== '' || input === '0' ? (
+            <NumberFormat value={input} displayType={'text'} thousandSeparator={true} />
+          ) : (
+            <NumberFormat value={preState} displayType={'text'} thousandSeparator={true} />
+          )}
+        </Result>
         <div className="">
           <div className="grid grid-cols-4 divide-x divide-y rounded-b-md">
-            <Button type="gray" click={clearInput}>
+            <Button type="gray" click={reset}>
               C
             </Button>
-            <Button type="gray" click={clickOperation}>
+            <Button type="gray" click={minusPlus}>
               +/-
             </Button>
-            <Button type="gray" click={clickOperation}>
+            <Button type="gray" click={percent}>
               %
             </Button>
-            <Button type="orange">&divide;</Button>
-            <Button type="ivory" click={clickNumbers}>
+            <Button type="orange" click={operatorType}>
+              /
+            </Button>
+            <Button type="ivory" click={inputNum}>
               7
             </Button>
-            <Button type="ivory" click={clickNumbers}>
+            <Button type="ivory" click={inputNum}>
               8
             </Button>
-            <Button type="ivory" click={clickNumbers}>
+            <Button type="ivory" click={inputNum}>
               9
             </Button>
-            <Button type="orange">&times;</Button>
-            <Button type="ivory" click={clickNumbers}>
+            <Button type="orange" click={operatorType}>
+              X
+            </Button>
+            <Button type="ivory" click={inputNum}>
               4
             </Button>
-            <Button type="ivory" click={clickNumbers}>
+            <Button type="ivory" click={inputNum}>
               5
             </Button>
-            <Button type="ivory" click={clickNumbers}>
+            <Button type="ivory" click={inputNum}>
               6
             </Button>
-            <Button type="orange">&minus;</Button>
-            <Button type="ivory" click={clickNumbers}>
+            <Button type="orange" click={operatorType}>
+              -
+            </Button>
+            <Button type="ivory" click={inputNum}>
               1
             </Button>
-            <Button type="ivory" click={clickNumbers}>
+            <Button type="ivory" click={inputNum}>
               2
             </Button>
-            <Button type="ivory" click={clickNumbers}>
+            <Button type="ivory" click={inputNum}>
               3
             </Button>
-            <Button type="orange">+</Button>
-            <Button className="numbers rounded-bl-md p-6" click={clickNumbers}>
+            <Button type="orange" click={operatorType}>
+              +
+            </Button>
+            <Button className="numbers rounded-bl-md p-6" click={inputNum}>
               0
             </Button>
-            <Button type="ivory" click={clickOperation}>
+            <Button type="ivory" click={inputNum}>
               .
             </Button>
             <Button type="ivory" click={backspace}>
               🥷
             </Button>
-            <Button type="darkOrange" click={performOperation}>
+            <Button type="darkOrange" click={equals}>
               =
             </Button>
           </div>
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Result = ({ value }) => {
+const Result = ({ children }) => {
   return (
     <div className="flex justify-end items-end text-5xl w-full result text-white rounded-t-md h-36 pr-2 pb-1">
-      {value}
+      {children}
     </div>
   );
 };
